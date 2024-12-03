89ee9141f1159bee46bcbc83157649cd72a55e9c
satyam.kushwaha <satyam.kushwaha@rtctek.com>
2024-12-03 14:49:01 +0530

    life cycle call back by using AbstractApplicationContext interface.

--- src/main/java/com/rtc/SpringCoreRtc/DrawingApp.java
+++ src/main/java/com/rtc/SpringCoreRtc/DrawingApp.java
@@ -1,6 +1,5 @@
 package com.rtc.SpringCoreRtc;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
