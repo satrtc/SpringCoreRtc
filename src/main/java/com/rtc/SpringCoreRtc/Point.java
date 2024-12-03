42e1f578a84a414d449a52512c1342fefd06d4c9
Satyam Kushwaha <satyam.kushwaha@rtctek.global>
2024-12-03 14:03:22 +0530

    Initial commit

---
+++ src/main/java/com/rtc/SpringCoreRtc/Point.java
@@ -0,0 +1,24 @@
+package com.rtc.SpringCoreRtc;
+
+public class Point {
+
+    public int x;
+    public int y;
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
---
+++ src/main/java/com/rtc/SpringCoreRtc/Triangle.java
@@ -0,0 +1,40 @@
+package com.rtc.SpringCoreRtc;
+
+
+public class Triangle {
+    public Point pointA;
+    public Point pointB;
+    public Point pointC;
+
+    public void draw()
+    {
+        System.out.println("Triangle drawn");
+        System.out.println("point A|"+"x:"+pointA.getX()+" y:"+pointA.getY());
+        System.out.println("point B|"+"x:"+pointB.getX()+" y:"+pointB.getY());
+        System.out.println("point C|"+"x:"+pointC.getX()+" y:"+pointC.getY());
+    }
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+}
